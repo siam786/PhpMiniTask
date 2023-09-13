892c5139b9049a5804fc87548a244e1717d04b23
siam786e <shohrab.hossain36@gmail.com>
2023-09-13 14:51:31 +0600

    add fixed

--- simple_calculator.php
+++ simple_calculator.php
@@ -12,6 +12,8 @@
     if(isset($_POST['submit'])){
         $num1=$_POST['num1'];
         $num2=$_POST['num2'];
+        $result=0;
+       
         $operation=$_POST['operation'];
         if($operation=='add'){
             $result=$num1+$num2;
@@ -19,7 +21,6 @@
         elseif($operation=='sub'){
       
             $result=$num1-$num2;
-            
         }
         elseif($operation=='mul'){
             $result=$num1*$num2;
@@ -27,7 +28,10 @@
         elseif($operation=='div'){
             $result=$num1/$num2;
         }
-        echo "<h2>Result: $result</h2>";
+            
+            echo "<h2 id='result'>Result: $result</h2>";
+       
+ 
     }
     ?>
     <!-- Build a PHP calculator named simple_calculator.php that performs basic arithmetic operations. Provide input fields -->
@@ -35,7 +39,7 @@
     the result of the chosen operation.
 </body> -->
     <h1>Simple Calculator</h1>
-    <form action="" method="POST">
+    <form id="form" method="POST">
         <input type="number" name="num1" placeholder="Enter first number" required><br>
         <input type="number" name="num2" placeholder="Enter second number" required><br>
         <select name="operation">
@@ -44,9 +48,20 @@
             <option value="mul">*</option>
             <option value="div">/</option>
         </select><br>
-        <input type="submit" name="submit" value="Submit">
-
-
+        <input id="showBtn" type="submit" name="submit" value="Submit">
     </form>
+    <!-- <script>
+    let result = document.getElementById("result");
+    let showBtn = document.getElementById("showBtn");
+    let form = document.getElementById("form");
+    form.addEventListener("submit", function(event) {
+        event.preventDefault()
+    })
+    result.style.display = "none";
+    showBtn.onclick = function() {
+        result.style.display = "block";
+
+    } -->
+    </script>
 
 </html>
